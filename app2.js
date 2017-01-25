182d7114576dbfc3cba390c31c96ab1d2c3075be
vifelisberto <vi.felis@hotmail.com>
2017-01-25 13:14:58 -0200

    Teste indexOf com list

--- app2.js
+++ app2.js
@@ -42,7 +42,11 @@
                                   continue
                           }
                           if (text.indexOf('cursos') != -1){
+                            sendTextMessage(sender, "Esses são nossos cursos ;)")
                             sendGenericMessage(sender)
+                          }
+                          if (text.indexOf('workshop') != -1){
+                            EnviaList(sender)
                           }
                           //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
                   }
@@ -130,6 +134,132 @@
                           console.log('Error: ', response.body.error)
                   }
           })
+  }
+
+  function EnviaList(sender) {
+    let messageData = {
+      "attachment": {
+              "type": "template",
+              "payload": {
+                  "template_type": "list",
+                  "elements": [
+                      {
+                          "title": "Classic T-Shirt Collection",
+                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                          "subtitle": "See all our colors",
+                          "default_action": {
+                              "type": "web_url",
+                              "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+                              "messenger_extensions": true,
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          "buttons": [
+                              {
+                                  "title": "View",
+                                  "type": "web_url",
+                                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                  "messenger_extensions": true,
+                                  "webview_height_ratio": "tall",
+                                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                              }
+                          ]
+                      },
+                      {
+                          "title": "Classic White T-Shirt",
+                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+                          "subtitle": "100% Cotton, 200% Comfortable",
+                          "default_action": {
+                              "type": "web_url",
+                              "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                              "messenger_extensions": true,
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          "buttons": [
+                              {
+                                  "title": "Shop Now",
+                                  "type": "web_url",
+                                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                                  "messenger_extensions": true,
+                                  "webview_height_ratio": "tall",
+                                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                              }
+                          ]
+                      },
+                      {
+                          "title": "Classic Blue T-Shirt",
+                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                          "subtitle": "100% Cotton, 200% Comfortable",
+                          "default_action": {
+                              "type": "web_url",
+                              "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                              "messenger_extensions": true,
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          "buttons": [
+                              {
+                                  "title": "Shop Now",
+                                  "type": "web_url",
+                                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                  "messenger_extensions": true,
+                                  "webview_height_ratio": "tall",
+                                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                              }
+                          ]
+                      },
+                      {
+                          "title": "Classic Black T-Shirt",
+                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+                          "subtitle": "100% Cotton, 200% Comfortable",
+                          "default_action": {
+                              "type": "web_url",
+                              "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                              "messenger_extensions": true,
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          "buttons": [
+                              {
+                                  "title": "Shop Now",
+                                  "type": "web_url",
+                                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                  "messenger_extensions": true,
+                                  "webview_height_ratio": "tall",
+                                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                              }
+                          ]
+                      }
+                  ],
+                   "buttons": [
+                      {
+                          "title": "View More",
+                          "type": "postback",
+                          "payload": "payload"
+                      }
+                  ]
+              }
+          }
+
+    }
+
+    request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:token},
+            method: 'POST',
+            json: {
+                    recipient: {id:sender},
+                    message: messageData,
+            }
+    }, function(error, response, body) {
+            if (error) {
+                    console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                    console.log('Error: ', response.body.error)
+            }
+    })
+}
   }
 
   // spin spin sugar
